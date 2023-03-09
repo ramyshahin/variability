fbe75f213bd43aef38eb79a1fbe236fb48c24feb
Ramy Shahin <r.shahin@computer.org>
2023-03-08 22:33:13 -0800

    using finset as the underlying set type in the CIA algorithm

--- src/cia.lean
+++ src/cia.lean
@@ -1,50 +1,77 @@
 -- CIA: Change Impact Assessment
 import data.set 
+import data.finset
 
 open set
 
 namespace CIA
 
+constant SysElCount: ℕ
+constant GSNElCount: ℕ
+def SysEl: Type := fin SysElCount
+def GSNEl: Type := fin GSNElCount
+
+--section
+
+@[derive decidable_eq]
 inductive Annotation : Type
 | Reuse
 | Recheck
 | Revise
-
-constant SysEl : Type
-constant GSNEl : Type
-
-def Sys : Type := set SysEl
+ 
+--variables (SysEl : Type) [finSysEl: fintype SysEl] [deqSysEl: decidable_eq SysEl]
+--variables (GSNEl : Type) [finGSNEl: fintype GSNEl] [deqGSNEl: decidable_eq GSNEl]
 
 @[reducible]
-def GSN : Type := set GSNEl
+def Sys : Type := finset SysEl
+
+--@[reducible, simp]
+--instance SysEl_has_union: has_union (finset SysEl) := @finset.has_union SysEl deqSysEl
 
 @[reducible]
-def TraceRel : Type := set (SysEl × GSNEl)
+def GSN : Type := finset GSNEl
 
-constant sliceSys   (s : Sys)  (es : set SysEl) : Sys
-constant sliceGSN_V (ac : GSN) (es : set GSNEl) : GSN
-constant sliceGSN_R (ac : GSN) (es : set GSNEl) : GSN
+@[reducible]
+def TraceRel : Type := finset (SysEl × GSNEl)
+
+variable sliceSys   (s : Sys)  (es : finset SysEl) : Sys
+variable sliceGSN_V (ac : GSN) (es : finset GSNEl) : GSN
+variable sliceGSN_R (ac : GSN) (es : finset GSNEl) : GSN
 
 structure Delta :=
-(add : set SysEl) (delete : set SysEl) (modify : set SysEl)
+(add : finset SysEl) (delete : finset SysEl) (modify : finset SysEl)
 
-open has_union
+def allElements (d: Delta) : finset SysEl :=
+     (d.add ∪ d.delete ∪ d.modify)
 
 def restrict (t : TraceRel) (d : Delta) : TraceRel :=
-    λ x, x.1 ∈ (d.add ∪ d.delete ∪ d.modify) ∧ x ∈ t
+    finset.filter (λx, x.1 ∈ allElements d) t
+    
+def trace (t : TraceRel) (es : finset SysEl) : finset GSNEl :=
+    finset.image (λx:(SysEl × GSNEl), x.2) 
+        (finset.filter (λ e:(SysEl × GSNEl), e.1 ∈ es) t)
 
-def trace (t : TraceRel) (es : set SysEl) : set GSNEl :=
-    λ e, ∃ s ∈ es, (s,e) ∈ t 
+def createAnnotation'   (recheck : finset GSNEl)
+                        (revise : finset GSNEl) 
+                        (el: GSNEl) 
+                        : Annotation :=
+@dite 
+    Annotation 
+    (el ∈ recheck) 
+    (finset.decidable_mem el recheck) 
+    (λ_, Annotation.Recheck)
+    (λ_, @dite 
+        Annotation
+        (el ∈ revise) 
+        (finset.decidable_mem el revise)
+        (λ_,  Annotation.Revise) 
+        (λ_, Annotation.Reuse))
 
 def createAnnotation    (g : GSN) 
-                        (recheck : set GSNEl)
-                        (revise : set GSNEl)
-                    : set (GSNEl × Annotation) :=
-    let ch := image (λ e, (e, Annotation.Recheck)) recheck,
-        rv := image (λ e, (e, Annotation.Revise)) revise,
-        ru := image (λ e, (e, Annotation.Reuse)) 
-                    (g \ (recheck ∪ revise))
-    in  ch ∪ rv ∪ ru
+                        (recheck : finset GSNEl)
+                        (revise : finset GSNEl) 
+                        : finset (GSNEl × Annotation) :=
+    finset.image (λx, (x, createAnnotation' recheck revise x)) g
 
 open Delta
 
@@ -53,7 +80,7 @@ def GSN_IA  (S  : Sys)
             (A  : GSN)
             (R  : TraceRel)
             (D  : Delta)
-            : set (GSNEl × Annotation) :=
+            : finset (GSNEl × Annotation) :=
     let R'          := restrict R D,
         C1dm        := sliceSys S (D.delete ∪ D.modify),
         C1am        := sliceSys S' (D.add ∪ D.modify),
@@ -62,5 +89,6 @@ def GSN_IA  (S  : Sys)
         C3Recheck1  := sliceGSN_V A C2Revise,
         C3Recheck2  := sliceGSN_R A (C2Recheck ∪ C3Recheck1)
     in  createAnnotation A C3Recheck2 C2Revise
-
+    
 end CIA
+
