943b3927f7da9d22526cc5ad4719a36188c72c10
Ramy Shahin <r.shahin@computer.org>
2024-01-17 23:24:03 -0500

    upgrading to Lean4

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «variability» where
+  -- add package configuration options here
+
+lean_lib «Variability» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «variability» where
+  root := `Main
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4"
